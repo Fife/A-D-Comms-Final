5657cd11705ea9b4cb09a46f654c6ede3f112cbe
Jacob Fifield <43118015+Fife@users.noreply.github.com>
2022-12-12 11:14:53 -0700

    Add files via upload

---
+++ spectrum.m
@@ -0,0 +1,9 @@
+function [freq, mag] = spectrum(v_t, fs, time_window)
+    Y = real(fft(v_t));      
+    L = length(time_window);             % Length of signal
+    P2 = abs(Y/L);
+    mag = P2(1:L/2+1);
+    mag(2:end-1) = 2*mag(2:end-1);
+    freq = fs*(0:(L/2))/L;
+    
+end
