8726ccedab4844108a8aca155c5e7623a286d746
Jacob Fifield <43118015+Fife@users.noreply.github.com>
2022-12-15 09:26:13 -0700

    Add files via upload

--- AmplitudeDemodulation.m
+++ AmplitudeDemodulation.m
@@ -7,22 +7,30 @@ function [vdsbd,vssbd,vlcd] = AmplitudeDemodulation(time_window, vdsb, vssb, vlc
     % dsb demodulation 
     vdsbd = carrier.*vdsb;
     vdsbd = lowpass(real(vdsbd), 2500, fs, ImpulseResponse="iir",Steepness=0.95);
-    subplot(3,1,1);
-    plot(vdsbd);
+    subplot(2,3,1);plot(time_window.*1000, vdsbd);
     title("Double sideband demodulation");
     xlabel("t(ms)")
     ylabel("v(t)")
 
+    [freq, mag] = spectrum(vdsbd, fs, time_window);
+    subplot(2,3,4);stem(freq(1:75), mag(1:75));
+    title("Single-Sided Spectrum of Demodulated DSB Signal")
+    xlabel("f (Hz)")
+    ylabel("|V(f)|")
+
     % vssb demodulation 
 
-    [freq, mag] = spectrum(vssb, fs, time_window);
-    mag = circshift(mag, -51);
-    [freq, mag] = spectrum(mag, fs, time_window);
-    subplot(3,1,2);
-    plot(freq, mag); 
-
+    subplot(2,3,2);plot(time_window.*1000, vdsbd)
+    title("Demodulated SSB Signal")
     xlabel("t(ms)")
     ylabel("v(t)")
+
+    [freq, mag] = spectrum(vdsbd, fs, time_window);
+    subplot(2,3,5);stem(freq(1:75), mag(1:75));
+    title("Single-Sided Spectrum of Demodulated SSB Signal")
+    xlabel("f (Hz)")
+    ylabel("|V(f)|")
+
 
     %stem(vssbd);
     % v_f = abs(fft(vssb));
@@ -36,13 +44,17 @@ function [vdsbd,vssbd,vlcd] = AmplitudeDemodulation(time_window, vdsb, vssb, vlc
     % plot(vssbd);
     %[frequency,Magnitude] = spectrum(vssbd,fs,time_window)
     %stem(frequency,Magnitude);
-    title("Single sideband demodulation")
+    title("Single-Sided Spectrum of Demodulated SSB Signal")
     
     % vlc demodulation 
     vlcd = vlc(1 : 32 : fs/100);
-    subplot(3,1,3);
-    plot(vlcd);
+    subplot(2,3,3);plot(vlcd);
     title("LC demodulation")
     xlabel("t(ms)")
     ylabel("v(t)")
+
+    subplot(2,3,6);stem(freq(1:50), mag(1:50));
+    title("Single-Sided Spectrum of Demodulated LC Signal")
+    xlabel("f (Hz)")
+    ylabel("|V(f)|")
 end
--- AmplitudeModulationJF.m
+++ AmplitudeModulationJF.m
@@ -9,7 +9,10 @@ function [vdsb, vssbu, vlc] = AmplitudeModulationJF(time_window, v_t, fs)
 
     % DSB AM
     vdsb = eval(real(carrier(time_window).*v_t));
-    subplot(2,3,1); plot(time_window, real(vdsb))
+    subplot(2,3,1); plot(time_window.*1000, real(vdsb))
+    title("Double Sideband Modulation")
+    xlabel("t(ms)")
+    ylabel("Volts")
     
     % SSB AM 
 
@@ -18,20 +21,35 @@ function [vdsb, vssbu, vlc] = AmplitudeModulationJF(time_window, v_t, fs)
     v_f([1551:end]) = 0;
     v_f = v_f .*0.002;
     vssbu = real(ifft(v_f));
-    
-
     %vssbu = highpass(real(vdsb), f_c-100, fs, ImpulseResponse="iir",Steepness=0.95);
-    subplot(2,3,2); plot(time_window, vssbu)
+    subplot(2,3,2); plot(time_window.*1000, vssbu)
+    title("Single Sideband Modulation")
+    xlabel("t(ms)")
+    ylabel("Volts")
+
     
     % Large Carrier AM 
     vlc = eval(carrier(time_window)) + 125*real(vdsb);
-    subplot(2,3,3); plot(time_window, real(vlc))
+    subplot(2,3,3); plot(time_window.*1000, real(vlc))
+    title("Large Carrier Modulation")
+    xlabel("t(ms)")
+    ylabel("Volts")
     
    
     %Spectrurm Creation
     subplot(2,3,4); FourierTransformJF(vdsb, fs, 2500, 7500)
-    
+    title("Single-Sided Spectrum of DSB Signal")
+    xlabel("f (Hz)")
+    ylabel("|V(f)|")
+
     subplot(2,3,5); FourierTransformJF(vssbu, fs, 2500, 7500)
-    
+    title("Single-Sided Spectrum of SSB Signal")
+    xlabel("f (Hz)")
+    ylabel("|V(f)|")
+
     subplot(2,3,6); FourierTransformJF(vlc, fs, 2500, 7500)
+    title("Single-Sided Spectrum of LC Signal")
+    xlabel("f (Hz)")
+    ylabel("|V(f)|")
+
 end
